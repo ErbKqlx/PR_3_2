ac375b91e334bf109e31c94cbf076fbd76614e79
Курзенева Кристина <kristinakurzeneva35@gmail.com>
2024-12-26 14:06:38 +0500

    Изменение внешнего вида формы

--- PR_3_2/FormMain.Designer.cs
+++ PR_3_2/FormMain.Designer.cs
@@ -29,105 +29,118 @@
         private void InitializeComponent()
         {
             panelMain = new Panel();
+            panel1 = new Panel();
             dataGridViewAll = new DataGridView();
-            panelButtons = new Panel();
-            button2 = new Button();
+            flowLayoutPanelButtons = new FlowLayoutPanel();
             button1 = new Button();
-            buttonSave = new Button();
+            button2 = new Button();
+            button3 = new Button();
             panelMain.SuspendLayout();
+            panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridViewAll).BeginInit();
-            panelButtons.SuspendLayout();
+            flowLayoutPanelButtons.SuspendLayout();
             SuspendLayout();
             // 
             // panelMain
             // 
-            panelMain.Controls.Add(dataGridViewAll);
+            panelMain.Controls.Add(panel1);
+            panelMain.Controls.Add(flowLayoutPanelButtons);
             panelMain.Dock = DockStyle.Bottom;
-            panelMain.Location = new Point(0, 87);
+            panelMain.Location = new Point(0, 0);
+            panelMain.Margin = new Padding(4);
             panelMain.Name = "panelMain";
-            panelMain.Padding = new Padding(10);
-            panelMain.Size = new Size(1032, 518);
+            panelMain.Size = new Size(1261, 610);
             panelMain.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(dataGridViewAll);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 86);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1261, 524);
+            panel1.TabIndex = 1;
             // 
             // dataGridViewAll
             // 
+            dataGridViewAll.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
             dataGridViewAll.BackgroundColor = Color.White;
             dataGridViewAll.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewAll.Dock = DockStyle.Fill;
-            dataGridViewAll.Location = new Point(10, 10);
+            dataGridViewAll.Location = new Point(0, 0);
+            dataGridViewAll.MultiSelect = false;
             dataGridViewAll.Name = "dataGridViewAll";
-            dataGridViewAll.Size = new Size(1012, 498);
+            dataGridViewAll.ReadOnly = true;
+            dataGridViewAll.Size = new Size(1261, 524);
             dataGridViewAll.TabIndex = 0;
             // 
-            // panelButtons
+            // flowLayoutPanelButtons
             // 
-            panelButtons.Controls.Add(button2);
-            panelButtons.Controls.Add(button1);
-            panelButtons.Controls.Add(buttonSave);
-            panelButtons.Dock = DockStyle.Top;
-            panelButtons.Location = new Point(0, 0);
-            panelButtons.Name = "panelButtons";
-            panelButtons.Size = new Size(1032, 81);
-            panelButtons.TabIndex = 4;
-            // 
-            // button2
-            // 
-            button2.AutoSize = true;
-            button2.BackColor = Color.White;
-            button2.Location = new Point(122, 15);
-            button2.Name = "button2";
-            button2.Size = new Size(127, 50);
-            button2.TabIndex = 2;
-            button2.Text = "Редактировать";
-            button2.UseVisualStyleBackColor = false;
+            flowLayoutPanelButtons.Controls.Add(button1);
+            flowLayoutPanelButtons.Controls.Add(button2);
+            flowLayoutPanelButtons.Controls.Add(button3);
+            flowLayoutPanelButtons.Dock = DockStyle.Top;
+            flowLayoutPanelButtons.Location = new Point(0, 0);
+            flowLayoutPanelButtons.Name = "flowLayoutPanelButtons";
+            flowLayoutPanelButtons.Padding = new Padding(10);
+            flowLayoutPanelButtons.Size = new Size(1261, 86);
+            flowLayoutPanelButtons.TabIndex = 0;
             // 
             // button1
             // 
-            button1.AutoSize = true;
             button1.BackColor = Color.White;
-            button1.Location = new Point(255, 15);
+            button1.Location = new Point(13, 13);
             button1.Name = "button1";
-            button1.Size = new Size(102, 50);
-            button1.TabIndex = 1;
-            button1.Text = "Удалить";
+            button1.Size = new Size(162, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Сохранить";
             button1.UseVisualStyleBackColor = false;
             // 
-            // buttonSave
+            // button2
             // 
-            buttonSave.AutoSize = true;
-            buttonSave.BackColor = Color.White;
-            buttonSave.Location = new Point(14, 15);
-            buttonSave.Name = "buttonSave";
-            buttonSave.Size = new Size(102, 50);
-            buttonSave.TabIndex = 0;
-            buttonSave.Text = "Сохранить";
-            buttonSave.UseVisualStyleBackColor = false;
+            button2.BackColor = Color.White;
+            button2.Location = new Point(181, 13);
+            button2.Name = "button2";
+            button2.Size = new Size(162, 42);
+            button2.TabIndex = 1;
+            button2.Text = "Редактировать";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.White;
+            button3.Location = new Point(349, 13);
+            button3.Name = "button3";
+            button3.Size = new Size(162, 42);
+            button3.TabIndex = 2;
+            button3.Text = "Удалить";
+            button3.UseVisualStyleBackColor = false;
             // 
             // FormMain
             // 
-            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1032, 605);
-            Controls.Add(panelButtons);
+            ClientSize = new Size(1261, 610);
             Controls.Add(panelMain);
-            Font = new Font("Segoe UI", 12F);
-            Margin = new Padding(4);
+            Font = new Font("Segoe UI", 14.25F);
+            Margin = new Padding(5);
             Name = "FormMain";
             Text = "Form1";
             panelMain.ResumeLayout(false);
+            panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dataGridViewAll).EndInit();
-            panelButtons.ResumeLayout(false);
-            panelButtons.PerformLayout();
+            flowLayoutPanelButtons.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
         private Panel panelMain;
-        private Panel panelButtons;
-        private Button buttonSave;
+        private Panel panel1;
+        private FlowLayoutPanel flowLayoutPanelButtons;
         private DataGridView dataGridViewAll;
-        private Button button2;
         private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
